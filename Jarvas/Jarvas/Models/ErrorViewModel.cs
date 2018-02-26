212a573de3ade0f4545c887b4ee60a902656d297
Emanuel Morais <emanuel.morais@farfetch.com>
2018-02-26 22:54:08 +0000

    first commit

---
+++ Jarvas/Jarvas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jarvas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
